1ce93e6c32db2b04f33cfe4748b233b055c2eaa5
amsico <amsicodd@gmail.com>
2022-01-29 00:39:26 +0100

    fix: until-testing

--- tests/widgets/test_utils.py
+++ tests/widgets/test_utils.py
@@ -3,4 +3,4 @@ from pyqtschema.widgets.utils import is_concrete_schema
 
 def test_is_concrete_schema():
     assert not is_concrete_schema({'non': 'sense'})
-    assert not is_concrete_schema({'type': 'string'})
+    assert is_concrete_schema({'type': 'string'})
