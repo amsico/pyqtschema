6dd7a06e42a6efe5f5e7299fac175d60a2bfeb22
amsico <amsicodd@gmail.com>
2022-01-31 00:13:13 +0100

    add anyOf test-reminder for changing the selection-combo when the any-of object should be changed

--- tests/widgets/test_group_anyof.py
+++ tests/widgets/test_group_anyof.py
@@ -30,3 +30,17 @@ def test_any_of_set_state(qtbot):
     widget.state = value
     assert widget.state == value
     assert widget.widgets['string'].state == value
+    assert widget.select_combo.currentIndex() == 0
+
+
+def test_any_of_set_state_switch(qtbot):
+    """ combo-change due to different type """
+    builder = WidgetBuilder(schema)
+    sub_schema = schema['properties']['value']
+    widget = AnyOfSchemaWidget(sub_schema, {}, builder)
+
+    value = 1
+    widget.state = value
+    assert widget.state == 1
+    assert widget.widgets['integer'].state == value
+    assert widget.select_combo.currentIndex() == 1
