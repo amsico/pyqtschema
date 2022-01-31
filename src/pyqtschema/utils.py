d4c3fe013f4958ad6809101902f6f97da5bd27c0
amsico <amsicodd@gmail.com>
2022-01-31 23:23:39 +0100

    add scroll-area to support large widgets

--- src/pyqtschema/utils.py
+++ src/pyqtschema/utils.py
@@ -1,14 +1,19 @@
 import sys
 from typing import Dict
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QScrollArea
 
 from pyqtschema.builder import WidgetBuilder
 
 
-def build_example(schema: Dict):
+def build_example(schema: Dict, scrollbar: bool = True):
     app = QApplication(sys.argv)
     builder = WidgetBuilder(schema)
-    form = builder.create_form()
-    form.show()
+    widget = form = builder.create_form()
+    if scrollbar:
+        widget = QScrollArea()
+        widget.setWidget(form)
+        widget.setWidgetResizable(True)
+
+    widget.show()
     app.exec_()
