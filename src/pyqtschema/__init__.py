b2e873b379fd6c9875fb8dd85b285e68004504f4
amsico <amsicodd@gmail.com>
2022-02-02 00:44:15 +0100

    add version to package

--- src/pyqtschema/__init__.py
+++ src/pyqtschema/__init__.py
@@ -1,2 +1,9 @@
 from .builder import WidgetBuilder
 from .utils import build_example
+
+VERSION: str = "0.1"
+
+
+def get_version() -> str:
+    """ the current status version """
+    return VERSION
