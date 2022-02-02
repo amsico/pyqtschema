c4fb01b9591c48fa2f00093d47fb83c669b3655e
amsico <david.regesch@stein.de>
2022-02-03 00:35:00 +0100

    add build_example_widget to simplify testing

--- src/pyqtschema/utils.py
+++ src/pyqtschema/utils.py
@@ -1,15 +1,20 @@
 import sys
 from typing import Dict
 
-from PyQt5.QtWidgets import QApplication, QScrollArea
+from PyQt5.QtWidgets import QApplication, QScrollArea, QWidget
 
 from pyqtschema.builder import WidgetBuilder
 
 
+def build_example_widget(schema: Dict, ui_schema=None) -> QWidget:
+    builder = WidgetBuilder(schema)
+    form = builder.create_form(ui_schema=ui_schema)
+    return form
+
+
 def build_example(schema: Dict, scrollbar: bool = True):
     app = QApplication(sys.argv)
-    builder = WidgetBuilder(schema)
-    widget = form = builder.create_form()
+    widget = form = build_example_widget(schema)
     if scrollbar:
         widget = QScrollArea()
         widget.setWidget(form)
