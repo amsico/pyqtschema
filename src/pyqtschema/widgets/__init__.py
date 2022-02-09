f6a8cbc53e364ff44e42f240c1c8d91dc024fa3a
amsico <amsicodd@gmail.com>
2022-02-09 22:41:02 +0100

    rename DirectorypathSchemaWidget to DirectoryPathSchemaWidget

--- src/pyqtschema/widgets/__init__.py
+++ src/pyqtschema/widgets/__init__.py
@@ -5,7 +5,7 @@ from .color import ColorSchemaWidget
 from .enu import EnumSchemaWidget
 from .groups import ObjectSchemaWidget
 from .numeric import SpinSchemaWidget, SpinDoubleSchemaWidget, IntegerRangeSchemaWidget
-from .paths import FilepathSchemaWidget, DirectorypathSchemaWidget
+from .paths import FilepathSchemaWidget, DirectoryPathSchemaWidget
 from .root import FormWidget
 from .text import TextSchemaWidget, TextAreaSchemaWidget, PasswordWidget
 
