a49f3ea082584abd25e842bff648230c14d52a51
amsico <amsicodd@gmail.com>
2022-02-11 00:25:26 +0100

    support Enums when writing json-files

--- src/pyqtschema/utils.py
+++ src/pyqtschema/utils.py
@@ -1,5 +1,7 @@
 import sys
-from typing import Dict
+from enum import Enum
+from json import JSONEncoder, dump
+from typing import Dict, Any
 
 from PyQt5.QtWidgets import QApplication, QScrollArea, QWidget
 
@@ -22,3 +24,18 @@ def build_example(schema: Dict, scrollbar: bool = True):
 
     widget.show()
     app.exec_()
+
+
+class Encoder(JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, Enum):
+            return o.value
+        return JSONEncoder.default(self, o)
+
+
+def json_dump(obj, fp, *, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan=True, indent=None,
+              separators=None, sort_keys=False, **kwargs):
+    """ adjusted json-dump to support Enum-values"""
+    kwargs['cls'] = Encoder
+    return dump(obj, fp, skipkeys=skipkeys, ensure_ascii=ensure_ascii, check_circular=check_circular,
+                allow_nan=allow_nan, indent=indent, separators=separators, sort_keys=sort_keys, **kwargs)
