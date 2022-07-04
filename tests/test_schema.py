7b00c3ea130c9c61f5f1574cb0d15ee0c5bb506d
amsico <amsicodd@gmail.com>
2022-07-04 22:34:52 +0200

    add test fo is_valid function in Schema class

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -14,3 +14,18 @@ def test_init_schema():
         "required": ["string"]
     }
     schema = Schema(_def)
+
+
+def test_schema_is_valid():
+    _def = {'type': 'object', 'properties': {'checker': {'type': 'string'}},
+            'schema': {'additionalProperties': False}, 'additionalProperties': False}
+    schema = Schema(_def)
+
+    assert schema.is_valid_data({'checker': 'a string'})
+    assert schema.is_valid_data({'checker': ''})
+
+    assert schema.is_valid_data({'checker': 1}) is False
+    assert schema.is_valid_data({'checker': 1.2}) is False
+
+    assert schema.is_valid_data({'invalid_name': 1}) is False
+    assert schema.is_valid_data({'invalid_name': "1"}) is False
