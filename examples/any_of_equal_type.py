514cdbc43d9b60c5b492c401210fca12256e1a96
amsico <amsicodd@gmail.com>
2022-07-04 22:44:21 +0200

    add new str/Path AnyOf example

---
+++ examples/any_of_equal_type.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import Union
+
+from pydantic import BaseModel, Field
+
+from pyqtschema import build_example
+
+
+class UnionIssue(BaseModel):
+    """ Exporter configuration """
+    # the str-schema is {type: string}
+    # the Path-schema is {type: string, format=path}
+    output: Union[str, Path] = Field(default=None)
+
+
+if __name__ == '__main__':
+    print(UnionIssue.schema())
+    build_example(UnionIssue.schema())
